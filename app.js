140fa04135c942429d9e350325fdb733e02cdbf3
Euheniya Vasilieva <barabashkatyt@rambler.ru>
2022-03-29 07:41:18 +0300

    test commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const RED = '#FF0000';
+
+
+const firstName = 'Ivan';
+const lastName = 'Ivanov';
+
+let index = 0;
+if (firstName === 'Ivan') {
+  index = 1;
+} else {
+  index = 2;
+}
+// alert(`firstName: ${firstName}; lastName: ${lastName}`);
